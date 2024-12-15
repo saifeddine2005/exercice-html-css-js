987b9a2d2a3022d7fa9e7c20dcd1166dcc255fde
saif-eddine <160881406+saifeddine2005@users.noreply.github.com>
2024-12-15 18:44:47 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,19 @@
+ const listeTuto = [
+    {
+    code: 111,
+    titre: "Initiation en Javascript",
+    description: "Tutoriel res)",
+    },
+    {
+    code: 112,
+    titre: "React Redux",
+    description: "Concepts de bdux/jsToulkit",
+    },
+    {
+    code: 115,
+    titre: "Algorithmique",
+    description:
+    "Premier pas es",
+    },
+    ];
+    export default listeTuto;
